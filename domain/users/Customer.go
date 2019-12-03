a47c5cd82250e85cc864e6272ffd2107421a5f65
ESPOIR <espoirditekemena@gmail.com>
2019-12-03 12:26:52 -0800

    Merge branch 'master' of https://github.com/ESPOIR-DITE/OKVS2
    # Conflicts:
    #	views/html/itemAdd/addItem.html

--- domain/users/Customer.go
+++ domain/users/Customer.go
@@ -7,7 +7,16 @@ type Customer struct {
 	Status  string `json:"status"`
 }
 type Address struct {
+	Id            string `json:"id"`
+	UserId        string `json:"userId"`
+	Address       string `json:"address"`
+	AddressTypeId string `json:"addressTypeId"`
+	PhoneNumber   string `json:"phoneNumber"`
+}
+type AddressHelper struct {
 	Id          string `json:"id"`
+	UserId      string `json:"userId"`
 	Address     string `json:"address"`
+	AddressType string `json:"addressType"`
 	PhoneNumber string `json:"phoneNumber"`
 }
